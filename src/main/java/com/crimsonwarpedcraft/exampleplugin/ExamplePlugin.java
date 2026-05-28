f1c613617bd9bf9373508514857d69d80217dc2c
wyattkaleugher-glitch <wyatt.kaleugher@student.montourschools.com>
2026-05-28 18:16:48 -0400

    Update ExamplePlugin.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
@@ -1,6 +1,7 @@
 package com.crimsonwarpedcraft.exampleplugin;
 
 import com.crimsonwarpedcraft.exampleplugin.commands.TechniqueCommands;
+import com.crimsonwarpedcraft.exampleplugin.data.DomainManager;
 import com.crimsonwarpedcraft.exampleplugin.data.ProfileManager;
 import com.crimsonwarpedcraft.exampleplugin.listeners.CombatListener;
 import com.crimsonwarpedcraft.exampleplugin.listeners.JoinListener;
@@ -9,11 +10,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ExamplePlugin extends JavaPlugin {
 
     private ProfileManager profileManager;
+    private DomainManager domainManager; // Added tracker reference slot
 
     @Override
     public void onEnable() {
         this.profileManager = new ProfileManager(this);
-        
+        this.domainManager = new DomainManager(); // Instantiated tracker
         
         saveDefaultConfig();
 
@@ -23,11 +25,10 @@ public class ExamplePlugin extends JavaPlugin {
             getCommand("jjk").setTabCompleter(handler);
         }
 
-        // Register both the Combat core AND the brand new Join setup!
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
         getServer().getPluginManager().registerEvents(new JoinListener(this), this);
         
-        getLogger().info("JJK Plugin Core fully initialized. First-join mechanics are active!");
+        getLogger().info("JJK Plugin Core fully initialized with Domain Clash tracking!");
     }
 
     @Override
@@ -37,5 +38,9 @@ public class ExamplePlugin extends JavaPlugin {
 
     public ProfileManager getProfileManager() {
         return profileManager;
+    }
+
+    public DomainManager getDomainManager() {
+        return domainManager;
     }
 }
