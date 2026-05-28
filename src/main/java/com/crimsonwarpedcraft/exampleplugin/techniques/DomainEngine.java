6eb649f3d8701cd577d80c81f230cdbcc2bb8db4
wyattkaleugher-glitch <wyatt.kaleugher@student.montourschools.com>
2026-05-28 18:03:39 -0400

    Update DomainEngine.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/techniques/DomainEngine.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/techniques/DomainEngine.java
@@ -3,12 +3,16 @@ package com.crimsonwarpedcraft.exampleplugin.techniques;
 import com.crimsonwarpedcraft.exampleplugin.data.PlayerProfile;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.persistence.PersistentDataType;
 
 public class DomainEngine {
     public static void launchDomain(Player player, PlayerProfile profile, FileConfiguration config) {
@@ -21,14 +25,25 @@ public class DomainEngine {
 
         player.sendMessage("§0§lDomain Expansion!!");
         Location center = player.getLocation();
+        
+        // Dynamic lookup to get our plugin instance for NamespacedKeys
+        JavaPlugin plugin = JavaPlugin.getProvidingPlugin(DomainEngine.class);
+        NamespacedKey barrierKey = new NamespacedKey(plugin, "domain_barrier");
 
-        // Physically build the boundary out of solid Black Concrete
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
                     double distance = Math.sqrt(x*x + y*y + z*z);
                     if (distance >= radius - 0.8 && distance <= radius + 0.5) {
-                        center.clone().add(x, y, z).getBlock().setType(Material.BLACK_CONCRETE);
+                        Block block = center.clone().add(x, y, z).getBlock();
+                        block.setType(Material.BLACK_CONCRETE);
+                        
+                        // SECURE DATA STAMP: Marks this specific concrete block as an unbreakable barrier
+                        block.getChunk().getPersistentDataContainer().set(
+                            new NamespacedKey(plugin, "domain_block_" + block.getX() + "_" + block.getY() + "_" + block.getZ()), 
+                            PersistentDataType.BYTE, 
+                            (byte) 1
+                        );
                     }
                 }
             }
@@ -37,7 +52,6 @@ public class DomainEngine {
         center.getWorld().spawnParticle(Particle.FLASH, center, 5, 2.0, 2.0, 2.0);
         center.getWorld().playSound(center, Sound.BLOCK_PORTAL_TRAVEL, 1.5F, 0.5F);
 
-        // Execute Sure-Hit payload calculations
         for (Entity entity : center.getWorld().getNearbyEntities(center, radius, radius, radius)) {
             if (entity instanceof LivingEntity target && !entity.equals(player)) {
                 target.damage(damage, player);
