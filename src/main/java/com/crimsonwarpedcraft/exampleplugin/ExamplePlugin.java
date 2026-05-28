fcefa5e4cafd73eacd4ffb8e047c5c4a8d6f8c1f
wyattkaleugher-glitch <wyatt.kaleugher@student.montourschools.com>
2026-05-28 18:02:09 -0400

    Update ExamplePlugin.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
@@ -3,6 +3,7 @@ package com.crimsonwarpedcraft.exampleplugin;
 import com.crimsonwarpedcraft.exampleplugin.commands.TechniqueCommands;
 import com.crimsonwarpedcraft.exampleplugin.data.ProfileManager;
 import com.crimsonwarpedcraft.exampleplugin.listeners.CombatListener;
+import com.crimsonwarpedcraft.exampleplugin.listeners.JoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExamplePlugin extends JavaPlugin {
@@ -15,15 +16,17 @@ public class ExamplePlugin extends JavaPlugin {
         
         saveDefaultConfig();
 
-        // Register the class as both the Command Executor AND the Tab Completer
         if (getCommand("jjk") != null) {
             TechniqueCommands handler = new TechniqueCommands(this);
             getCommand("jjk").setExecutor(handler);
             getCommand("jjk").setTabCompleter(handler);
         }
 
+        // Register both the Combat core AND the brand new Join setup!
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
-        getLogger().info("JJK Plugin Core fully initialized with custom Tab-Completion suggestions!");
+        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        
+        getLogger().info("JJK Plugin Core fully initialized. First-join mechanics are active!");
     }
 
     @Override
