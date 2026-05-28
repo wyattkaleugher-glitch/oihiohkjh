8643d9e86840a872e691ae56dd9db3b08db70016
wyattkaleugher-glitch <wyatt.kaleugher@student.montourschools.com>
2026-05-28 12:04:54 -0400

    Update TechniqueType.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/data/TechniqueType.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/data/TechniqueType.java
@@ -1,1 +1,15 @@
+package com.crimsonwarpedcraft.exampleplugin.data;
 
+/**
+ * Defines all available Innate Cursed Techniques 
+ * directly within the core data package layer.
+ */
+public enum TechniqueType {
+    NONE,
+    GOJO,
+    SUKUNA,
+    MEGUMI,
+    YUJI,
+    INUMAKI,
+    MAHITO
+}
